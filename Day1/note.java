7f6bb495ffe2be25ee075cc1ee8b26420c66f54d
arisia <starlights2283@gmail.com>
2023-09-07 16:18:33 +0900

    add day1

---
+++ Day1/note.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class note {
+    public static void main(String [] args) {
+        // main 메서드 진입하기
+        System.out.println("Hello Java");
+        int x;
+        x = 15;
+        int y;
+        y = 15;
+        int result = x + y;
+        if(result == 30){
+            System.out.println("data = " + result);
+        }else {
+            System.out.println("data not equal");
+        }
+        return;
+    }
+}
