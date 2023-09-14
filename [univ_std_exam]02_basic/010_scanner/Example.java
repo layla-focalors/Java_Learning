cf2e2043ac5d1538726741321c1cec4eed07d566
arisia <starlights2283@gmail.com>
2023-09-14 15:19:53 +0900

    Add try-except

--- [univ_std_exam]02_basic/010_scanner/Example.java
+++ [univ_std_exam]02_basic/010_scanner/Example.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        // ------------------------------------------------------------------
-        // 010_scanner 예제의 목표는
-        // Scanner 클래스를 사용하여 표준 입력으로 자료형 별로 값을 입력받는 실습한다.
-        // ------------------------------------------------------------------
-        Scanner scan = new Scanner(System.in);
-        System.out.println("원하는 키워드 입력 \n");
-        String reade = scan.nextLine();
-        System.out.printf("inputString : %s", reade);
+        try (// ------------------------------------------------------------------
+                // 010_scanner 예제의 목표는
+                // Scanner 클래스를 사용하여 표준 입력으로 자료형 별로 값을 입력받는 실습한다.
+                // ------------------------------------------------------------------
+        Scanner scan = new Scanner(System.in)) {
+            System.out.println("원하는 키워드 입력 \n");
+            String reade = scan.nextLine();
+            System.out.printf("inputString : %s", reade);
+        }
         // System.out.println(scan.nextLine());
         return;
     }
