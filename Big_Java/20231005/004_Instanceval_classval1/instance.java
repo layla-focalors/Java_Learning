ca3def5f39d234edc9f29909b8703bfc82faaa34
arisia <starlights2283@gmail.com>
2023-10-05 15:16:17 +0900

    add update

--- Big_Java/20231005/004_Instanceval_classval1/instance.java
+++ Big_Java/20231005/004_Instanceval_classval1/instance.java
@@ -2,9 +2,39 @@
 
 public class instance {
     public static void main(String[] args){
+        System.out.println(TV_A134.width);   // 클래스 변수는 인스턴스 생성 전에도 접근 가능
+        System.out.println(TV_A134.height);  // 클래스 변수
+
         TV_A134 t = new TV_A134();
-        t.Setcolor("violet");
-        System.out.printf("tv color is : %s", t.color);
+        t.Setcolor("violet"); 
+        System.out.printf("tv color is : %s\n", t.color);
+
+        // 컬러를 red로 지정하여 인스턴스 생성
+        TV_A134 red_t;
+        red_t = new TV_A134();
+        red_t.Setcolor("red");
+
+        // 컬러를 blue로 지정하여 인스턴스 생성
+        TV_A134 blue_t;
+        blue_t = new TV_A134();
+        blue_t.Setcolor("blue");
+
+        // non-static 변수는 각 인스턴스마다 독립적으로 저장됨
+        System.out.println(red_t.getcolor());
+        System.out.println(blue_t.getcolor());
+
+        // 인스턴스를 생성하고 width와 height 출력
+        System.out.println("red_t > " + red_t.width + "/" + red_t.height);
+        System.out.println("blue_t > " + blue_t.width + "/" + blue_t.height);
+
+        // 각 인스턴스마다 클래스 변수 수정
+        red_t.width = 11;
+        blue_t.height = 22;
+
+        // 클래스 변수는 모든 동일 객체들이 공유
+        System.out.println("red_t > " + red_t.width + "/" + red_t.height);
+        System.out.println("blue_t > " + blue_t.width + "/" + blue_t.height);
+
     }
 }
 class TV_A134{
