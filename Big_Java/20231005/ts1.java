ef710e3b9a4cd18b6be3fff256b64c5d1351c585
arisia <starlights2283@gmail.com>
2023-10-05 13:53:49 +0900

    Add sys

--- Big_Java/20231005/ts1.java
+++ Big_Java/20231005/ts1.java
@@ -4,6 +4,11 @@ public class ts1 {
     public static void main(String[] args) {
         int[] stad = {10, 20, 30};
         System.out.println(stad);
+        System.out.println(subset(12, 18));
         return;
+    }
+    public static int subset(int a, int b){
+        int c = a + b;
+        return c;
     }
 }
